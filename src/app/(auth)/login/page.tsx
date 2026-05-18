0dc261a1e01e35b2e0e672109d9fad104839c49b
CepilloTirolesa <contactocepillo@gmail.com>
2026-05-18 17:56:39 -0300

    Remove unused login import

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
