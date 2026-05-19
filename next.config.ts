5ac1b86011e570f67398388f0183dbcfea81e82e
CepilloTirolesa <contactocepillo@gmail.com>
2026-05-19 08:30:10 -0300

    Allow BVCT remote logo images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
 	experimental: {
 		devtoolSegmentExplorer: false,
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'bomberosvoluntarioscoloniatirolesa.org',
+				pathname: '/Images/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
